c225b9263b0ef44a2c76b9fab37f8982ab63cb02
Caeden Statia <caeden.s@outlook.com>
2023-01-06 16:01:26 -0800

    Item class

---
+++ Assets/Runtime/Items/Item.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Lunaculture.Items
+{
+    [CreateAssetMenu(fileName = "New Item", menuName = "Lunaculture/Item")]
+    public class Item : ScriptableObject
+    {
+        [field: Header("Basic Item Properties")]
+        [field: SerializeField] public Sprite Icon { get; private set; }
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public string Tooltip { get; private set;  }
+
+        [field: Header("Money!")]
+        [field: SerializeField] public bool CanBuy { get; private set; }
+        [field: SerializeField] public int BuyPrice { get; private set; }
+
+        [field: SerializeField] public bool CanSell { get; private set; }
+        [field: SerializeField] public int SellPrice { get; private set; }
+    }
+}
