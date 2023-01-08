a11239a16c8d6474dd7b332d4b514ddd3f1dd5b7
Caeden Statia <caeden.s@outlook.com>
2023-01-08 14:33:35 -0800

    Unsellable items are grayed out

---
+++ Assets/Runtime/ColorExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Lunaculture
+{
+    public static class ColorExtensions
+    {
+        public static Color WithAlpha(this Color color, float alpha)
+            => new(color.r, color.g, color.b, alpha);
+    }
+}
