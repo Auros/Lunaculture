54d5b7be9ff3d690d80d2f1df3171a63236e0882
Caeden Statia <caeden.s@outlook.com>
2023-01-07 23:21:06 -0800

    passive resource generation

--- Assets/Runtime/Machines/Miner/PhysicalMinerController.cs
+++ Assets/Runtime/Machines/Miner/PhysicalMinerController.cs
@@ -1,5 +1,7 @@
 ﻿using Lunaculture.Grids;
+using Lunaculture.Items;
 using Lunaculture.Placement;
+using System;
 using UnityEngine;
 
 namespace Lunaculture.Machines.Miner
@@ -23,7 +25,21 @@ namespace Lunaculture.Machines.Miner
 
         [SerializeField]
         private PhysicalMinerController _template = null!;
-        
+
+        [SerializeField]
+        private Item _generatedResource;
+
+        [Header("Generation rate is measured in seconds.")]
+        [SerializeField]
+        private float _generationRate = 1.0f;
+
+        [SerializeField]
+        private int _maxResourceSize = 100;
+
+        private float _generationTime = 0;
+        private int _currentItems = 0;
+
+
         private void Start()
         {
             Move();
@@ -42,7 +58,11 @@ namespace Lunaculture.Machines.Miner
             cell =>
             {
                 Place();
-                Instantiate(_template);
+
+                if (_template is not null)
+                {
+                    Instantiate(_template);
+                }
             });
         }
 
@@ -50,6 +70,20 @@ namespace Lunaculture.Machines.Miner
         {
             _visualContainer.SetActive(true);
             _overlapDetector.enabled = false;
+            
+            // TODO: Better cancel item generation while miner is in placement mode
+            _currentItems = 0;
+        }
+
+        private void Update()
+        {
+            _generationTime += Time.deltaTime * Time.timeScale;
+
+            if (_generationTime > _generationRate)
+            {
+                _generationTime = 0;
+                _currentItems = Math.Min(_currentItems + 1, _maxResourceSize);
+            }
         }
     }
 }
