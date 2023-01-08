d68b4748e8717c7786aa5acaec0fc37d2624f6d9
legoandmars <legoandmars@gmail.com>
2023-01-08 14:15:28 -0800

    Add flashing water icon when plant is unwatered + add hard shadows

---
+++ Assets/Runtime/Plants/PlantStatusEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Lunaculture.Plants
+{
+    public struct PlantStatusEvent
+    {
+        public PlantGrowthStatus GrowthStatus { get; private set; }
+
+        public PlantStatusEvent(PlantGrowthStatus growthStatus)
+        {
+            GrowthStatus = growthStatus;
+        }
+    }
+}
