385756bb7427e8e7ad0e7822404c65ae08196707
legoandmars <legoandmars@gmail.com>
2023-01-08 13:00:39 -0800

    Initial Player Model

--- Assets/Runtime/Player/PlayerController.cs
+++ Assets/Runtime/Player/PlayerController.cs
@@ -16,6 +16,16 @@ namespace Lunaculture.Player
 
         private Vector3 intendedVelocity = Vector3.zero;
 
+        [SerializeField]
+        private Animator _animator = null!;
+
+        [SerializeField]
+        private Transform RotatedObject = null!;
+
+        private bool walking = false;
+
+        private int _cachedWalkingPropertyId = 0;
+
         public void OnMovement(InputAction.CallbackContext context)
         {
             _ = outsideCollider;
@@ -24,17 +34,39 @@ namespace Lunaculture.Player
             var normalizedVelocity = velocity2d.normalized;
 
             intendedVelocity = movementSpeed * new Vector3(normalizedVelocity.x, 0, normalizedVelocity.y);
+
+            if (intendedVelocity != Vector3.zero)
+            {
+                _animator.SetBool("Walking", true);
+            }
         }
 
         private void FixedUpdate()
         {
             _rigidbody.velocity = intendedVelocity;
+
+            var currentlyWalking = _rigidbody.velocity != Vector3.zero;
+            if (currentlyWalking != walking)
+            {
+                walking = currentlyWalking;
+                _animator.SetBool(_cachedWalkingPropertyId, walking);
+            }
+
+            if (_rigidbody.velocity.magnitude > .01)
+            {
+                RotatedObject.rotation = Quaternion.LookRotation(_rigidbody.velocity, Vector3.up);
+            }
         }
 
         // TODO(Caeden): Perhaps rethink how to lock player rotation when we add a model
         private void LateUpdate()
         {
             transform.rotation = Quaternion.identity;
+        }
+
+        private void Start()
+        {
+            _cachedWalkingPropertyId = Animator.StringToHash("Walking");
         }
     }
 }
