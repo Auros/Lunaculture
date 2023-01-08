6ba273f5ee095839ddb38cf0df855a76e321f49a
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-01-07 19:57:39 -0500

    Plot Controller Mockup

---
+++ Assets/Runtime/Planting/Plots/PhysicalPlotController.cs
@@ -0,0 +1,24 @@
+﻿using Lunaculture.Grids;
+using Lunaculture.Placement;
+using UnityEngine;
+
+namespace Lunaculture.Planting.Plots
+{
+    public class PhysicalPlotController : MonoBehaviour
+    {
+        [SerializeField]
+        private GridController _gridController = null!;
+
+        [SerializeField]
+        private GridSelectionController _gridSelectionController = null!;
+
+        [SerializeField]
+        private GridPlaceable _placeable = null!;
+
+        [SerializeField]
+        private GameObject _visualContainer = null!;
+
+        [SerializeField]
+        private PlaceableObjectOverlapChecker _overlapChecker = null!;
+    }
+}
