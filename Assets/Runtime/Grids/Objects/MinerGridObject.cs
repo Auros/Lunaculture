1220e19c99640056e84146f4059e73c381e6ac8b
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-01-08 18:57:03 -0500

    Miner placement and resource collection

--- Assets/Runtime/Grids/Objects/MinerGridObject.cs
+++ Assets/Runtime/Grids/Objects/MinerGridObject.cs
@@ -1,7 +1,9 @@
-﻿namespace Lunaculture.Grids.Objects
+﻿using Lunaculture.Machines.Miner;
+
+namespace Lunaculture.Grids.Objects
 {
     public class MinerGridObject : GridObject
     {
-        
+        public PhysicalMinerController Controller { get; set; } = null!;
     }
 }
