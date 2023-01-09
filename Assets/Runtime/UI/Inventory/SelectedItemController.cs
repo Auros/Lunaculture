eb8cc50e768c7bdeecf414eb6ee686d0b3ce4765
Caeden Statia <caeden.s@outlook.com>
2023-01-08 20:04:26 -0800

    show selected item on bottom left

---
+++ Assets/Runtime/UI/Inventory/SelectedItemController.cs
@@ -0,0 +1,50 @@
+using Lunaculture.Player.Inventory;
+using TMPro;
+using UnityEngine;
+
+namespace Lunaculture.UI.Inventory
+{
+    public class SelectedItemController : MonoBehaviour
+    {
+        [SerializeField] private GameUIInterconnect gameUIInterconnect = null!;
+        [SerializeField] private TextMeshProUGUI selectedItemText = null!;
+
+        private InventoryService inventoryService = null!;
+
+        private void Start()
+        {
+            inventoryService = gameUIInterconnect.InventoryService;
+
+            inventoryService.InventoryUpdatedEvent += InventoryService_InventoryUpdatedEvent;
+        }
+
+        private void InventoryService_InventoryUpdatedEvent()
+        {
+            // lmao this sucks but oh well
+            var selectedItem = inventoryService.SelectedItem;
+
+            if (selectedItem != null)
+            {
+                var count = 1;
+                for (var i = 0; i < inventoryService.Inventory.Length; i++)
+                {
+                    if (inventoryService.Inventory[i] != null && inventoryService.Inventory[i].ItemType == selectedItem)
+                    {
+                        count = inventoryService.Inventory[i].Count;
+                    }
+                }
+
+                selectedItemText.text = $"Selected: {selectedItem.Name} x{count}";
+            }
+            else
+            {
+                selectedItemText.text = "Selected: None";
+            }
+        }
+
+        private void OnDestroy()
+        {
+            inventoryService.InventoryUpdatedEvent -= InventoryService_InventoryUpdatedEvent;
+        }
+    }
+}
